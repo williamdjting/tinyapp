236aae5059e502afdceee4d6fa6c6833ce9c4fcd
William <williamdjting@gmail.com>
2020-07-07 01:18:50 +0000

    updatefile

--- express_server.js
+++ express_server.js
@@ -24,3 +24,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
