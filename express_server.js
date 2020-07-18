49b854971a7703277af8cc7999704665df884dae
William Ting <willting@Williams-MacBook-Pro-2.local>
2020-07-17 19:49:19 -0700

    updated code style with more comments

--- express_server.js
+++ express_server.js
@@ -16,28 +16,16 @@ app.use(cookieSession({
   keys: ['key1'],
 }))
 
+//sets up bcrypt hashing
+const bcrypt = require('bcrypt');
 
-const bcrypt = require('bcrypt'); //sets up bcrypt hashing
-
-const helper = require("./helpers.js"); //imports helper.js
+//imports helper.js
+const helper = require("./helpers.js");
 
 //users object, stores the user information
 const users = {
 
 }
-
-// const users = { 
-//   "userRandomID": {
-//     id: "userRandomID", 
-//     email: "user@example.com", 
-//     password: "purple-monkey-dinosaur"
-//   },
-//  "user2RandomID": {
-//     id: "user2RandomID", 
-//     email: "user2@example.com", 
-//     password: "dishwasher-funk"
-//   }
-// }
 
 //database to store urls
 const newUrlDatabase = {
@@ -45,25 +33,28 @@ const newUrlDatabase = {
 };
 
 app.get("/", (req, res) => {
+  //redirects to register page
   res.redirect('/register');
 });
 
 //main page that shows urls
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: helper.urlsForUser(req.session.user_id, newUrlDatabase), username: req.session.user_id };
+  //if user does not have cookies, advise to register
   if (!(req.session.user_id)) {
     res.status(400).send("Please login or register first");
   } else {
+    let templateVars = { urls: helper.urlsForUser(req.session.user_id, newUrlDatabase), username: req.session.user_id };
     res.render("urls_index", templateVars);
   };
 });
 
 //creates new url for user
 app.get("/urls/new", (req, res) => {
-  let templateVars = { urls: newUrlDatabase, username: req.session.email };
+  //if user does not have cookies, advise to register
   if (!(req.session.user_id)) {
     res.redirect("/login");
   } else {
+    let templateVars = { urls: newUrlDatabase, username: req.session.user_id };
     res.render("urls_new", templateVars);
   }
 });
@@ -76,30 +67,33 @@ app.get("/urls/:shortURL", (req, res) => {
     return;
   }
   const record = newUrlDatabase[shortURL];
-  console.log(record);
+  //find the unique ID and if it is not in database, advise user
   if (!record) {
     res.status(400).send("Record not found.");
     return
   }
+
+  //if current userID is not same as session cookie, advise user
   if (record.userID !== req.session.user_id) {
     res.status(404).send("You don't own this ID.");
     return;
   }
+  // assign new ID to the actual URL in the database
   const longURL = newUrlDatabase[shortURL].longURL
   let templateVars = { shortURL: shortURL, longURL: longURL, username: req.session.user_id };
-  if (longURL) {
-    res.render("urls_show", templateVars);
-  } else {
-    res.render("urls_new", templateVars);
-  }
+  res.render("urls_show", templateVars);
+
 });
 
 //checks if individual has access to urls page if they have cookies
 app.post("/urls", (req, res) => {
+  //if user does not have cookies, advise to register
   if (!(req.session.user_id)) {
     res.status(400).send("Please login or register to access page");
   } else {
+    //generate a random string
     const shortURL = helper.generateRandomString();
+    //create new object with unique ID in database with the below parameters
     newUrlDatabase[shortURL] = {
       longURL: req.body.longURL,
       userID: req.session.user_id
@@ -112,10 +106,12 @@ app.post("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   shortURL = req.params.shortURL;
   const record = newUrlDatabase[shortURL];
+  //find the unique ID and if it is not in database, advise user
   if (!record) {
     res.status(400).send("Record not found.");
     return
   }
+  // assign new ID to the actual URL in the database
   const longURL = newUrlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
@@ -124,11 +120,14 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   const record = newUrlDatabase[shortURL];
+  //find the unique ID and if it is not in database, advise user
   if (!record) {
     res.status(400).send("Record not found.");
+    //if current userID is not same as session cookie, advise user
   } else if (record.userID !== req.session.user_id) {
     res.status(404).send("You don't own this ID.");
   } else {
+    // delete the ID from database
     delete newUrlDatabase[shortURL];
     res.redirect("/urls");
   }
@@ -136,26 +135,30 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //sends to the dynamic id link and its url
 app.post("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
+  const shortURL = req.params.shortURL; //
   const longURL = req.body.longURL;
+  //if user does not have cookies, advise to login
   if (!(req.session.user_id)) {
     res.status(400).send("Not logged in");
     return;
   }
   const record = newUrlDatabase[shortURL];
+  //find the unique ID and if it is not in database, advise user
   if (!record) {
     res.status(400).send("Record not found.");
     return
   }
+  //if current userID is not same as session cookie, advise user
   if (record.userID !== req.session.user_id) {
     res.status(404).send("You don't own this ID.");
     return;
   }
+  // assign new ID to the actual URL in the database
   newUrlDatabase[shortURL].longURL = longURL;
   res.redirect(`/urls`);
 });
 
-//logs out the user
+//when logged out, set session cookie to null and redirect to login screen
 app.post("/logout", (req, res) => {
   req.session.user_id = null
   res.redirect("/login");
@@ -163,8 +166,9 @@ app.post("/logout", (req, res) => {
 
 //checks the registration 
 app.get("/register", (req, res) => {
-  let templateVars = { urls: newUrlDatabase, username: req.session.user_id };
+  //if user does not have cookies, advise to register
   if (!(req.session.user_id)) {
+    let templateVars = { urls: newUrlDatabase, username: req.session.user_id };
     res.render("urls_register", templateVars);
   } else {
     res.redirect("/urls");
@@ -173,18 +177,21 @@ app.get("/register", (req, res) => {
 
 //checks if the passed in information exists already for registration purposes
 app.post("/register", (req, res) => {
-  
+  //finds the email of user object
   const email = req.body.email;
+  //finds the password of user object
   const password = req.body.password;
+  //if either parameters are empty then cannot register
   if (password === '' || email === '') {
-    res.status(400).send("Sorry! You can't see that.")
+    res.status(400).send("Please enter your email/password fields.")
     return;
   }
+  //if email is already in database then tell user
   if (helper.getUserByEmail(email, users)) {
-    res.status(403).send("Sorry! You can't see that.")
+    res.status(403).send("This email is already used.")
     return;
   }
-
+  //generates an ID for new user and an encrypted password and store into users database then assign current cookie to this new ID
   const id = helper.generateRandomString();
   const hashedPassword = bcrypt.hashSync(password, 10);
   users[id] = { id: id, email: email, password: hashedPassword };
@@ -196,8 +203,9 @@ app.post("/register", (req, res) => {
 
 //checks the login data
 app.get("/login", (req, res) => {
-  let templateVars = { urls: newUrlDatabase, username: req.session.user_id };
+  //if user does not have cookies, redirect to login page
   if (!(req.session.user_id)) {
+    let templateVars = { urls: newUrlDatabase, username: req.session.user_id };
     res.render("urls_login", templateVars);
   } else {
     res.redirect("/urls");
@@ -206,14 +214,20 @@ app.get("/login", (req, res) => {
 
 //checks the login data to confirm if it is valid
 app.post("/login", (req, res) => {
-  const email = req.body.username;
-  const password = req.body.password;
-  const validUser = helper.getUserByEmail(email, users);
+  //finds the email of user object
+  const email = req.body.username; 
+  //finds the password of user object
+  const password = req.body.password; 
+  //checks getUserByEmail function to see if email that is passed matches an email in database
+  const validUser = helper.getUserByEmail(email, users); 
   let checker = false;
-  if (validUser) {
+
+  //if there is a match, then give checker a hashed password
+  if (validUser) { 
     checker = bcrypt.compareSync(password, validUser.password);
   }
-  if (checker) {
+  //set cookie as existing ID from user's email and redirect
+  if (checker) { 
     req.session.user_id = validUser.id
     res.redirect(`/urls/`);
   } else {
@@ -221,8 +235,7 @@ app.post("/login", (req, res) => {
   }
 });
 
-
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`); // leaving to confirm that server is up
+  // leaving to confirm that server is up
+  console.log(`Example app listening on port ${PORT}!`); 
 });
--- helpers.js
+++ helpers.js
@@ -1,5 +1,4 @@
-//fetches user object from user database based off email
-
+//takes in the email and compares it with the emails in the users database to see if there is a match, if so pull the user object from users database with that email, otherwise return undefined
 function getUserByEmail(email, users) {
   for (let user in users) {
     if (users[user].email === email) {
@@ -9,7 +8,9 @@ function getUserByEmail(email, users) {
   return undefined;
 }
 
-//finds only the urls that belong to this users' id
+//looks into the newUrl database and comapares if the current ID passed in matches any of the userID in the newUrldatabase, if so, assign this new ID's object to the existing ID in newUrl Database
+
+finds only the urls that belong to this users' id
 function urlsForUser(id, newUrlDatabase) {
   let newObj = {};
   for (let shortURL in newUrlDatabase) {
@@ -20,15 +21,13 @@ function urlsForUser(id, newUrlDatabase) {
   return newObj;
 }
 
-// generatesRandomString id
+// generates a random string for creating unique ID's with
 function generateRandomString() {
   shortURL = (Math.random().toString(36).substring(7));
   return shortURL;
 }
 
 
-
-
-
+//exports functions to other files
 module.exports = { getUserByEmail, urlsForUser, generateRandomString }
 
